814613ee2c803b6a77b0223f2088166ba50beb62
Konrad Abicht <k.abicht@googlemail.com>
2017-01-03 18:12:54 +0100

    lat+long as float; added additional fields for coordinates file
    related to #1 and #3

--- create-building-coordinates-file.php
+++ create-building-coordinates-file.php
@@ -11,9 +11,37 @@ foreach ($buildingArray as $key => $value) {
     $lat = $value[11];
     $id = $value[12];
 
+    //
+    $entranceSuitableForWheelchair = 'vollständig' == $value[2]
+        ? 2
+        : 'teilweise' == $value[2]
+            ? 1
+            : 0;
+
+    //
+    $toiletSuitableForWheelchair = 'vollständig' == $value[5]
+        ? 2
+        : 'teilweise' == $value[5]
+            ? 1
+            : 0;
+
+    //
+    $parkingForDisabledPeople = 0 < (int)$value[15] || 0 < (int)$value[17]
+        ? 1 : 0;
+
     $buildingIndexArray[$id] = array(
+        'title' => $value[1],
         'longitude' => (float)$long,
         'latitude' => (float)$lat,
+        'entrance-suit-f-wheelchair' => $entranceSuitableForWheelchair,
+        'lift-suit-f-wheelchair' => 'ja' == $value[3] ? 1 : 0,
+        'lift-avail' => 'ja' == $value[4] ? 1 : 0,
+        'toilet-suit-f-wheelchair' => $toiletSuitableForWheelchair,
+        'parking-avail' => 'ja' == $value[13] ? 1 : 0,
+        'parking-f-disabled-avail' => $parkingForDisabledPeople,
+        'help-for-hearing-imp' => 'ja' == $value[75] ? 1 : 0,
+        'help-for-blind' => 'ja' == $value[76] ? 1 : 0,
+        'general-help' => 'ja' == $value[77] ? 1 : 0,
     );
 }
 
